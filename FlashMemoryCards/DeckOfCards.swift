b5c0f518c923629321f5e1c12193019eb46ffc4e
Jorge Guerra <jorgeguerra@DAL-38R-2017s-iMac.local>
2017-08-08 21:51:55 -0500

    Changed app icon and added all countries in Asia

--- FlashMemoryCards/DeckOfCards.swift
+++ FlashMemoryCards/DeckOfCards.swift
@@ -10,7 +10,7 @@ import Foundation
 
 struct DeckOfCards {
 
-    var nameList = ["Afghanistan","Armenia","Azerbaijan","Bahrain","Bangladesh","Bhutan","Brunei","Cambodia","China","Cyprus","Georgia","India"]
+    var nameList = ["Afghanistan","Armenia","Azerbaijan","Bahrain","Bangladesh","Bhutan","Brunei","Cambodia","China","Cyprus","Georgia","India","Indonesia","Iran","Iraq","Israel","Japan","Jordan","Kazakhstan","Kuwait","Kyrgyzstan","Laos","Lebanon","Malaysia","Maldives","Mongolia","Myanmar","Nepal","North Korea","Oman","Pakistan","Palestine","Philippines","Qatar","Russia","Saudi Arabia","Singapore","South Korea", "Sri Lanka","Syria","Taiwan","Tajikistan","Thailand","Timor-Leste","Turkey","Turkmenistan","United Arab Emirates","Uzbekistan","Vietnam","Yemen"]
 
     var dealtCards : [String] = []
     
@@ -41,4 +41,4 @@ struct DeckOfCards {
 
 
 
-//"Indonesia","Iran","Iraq", "Israel","Japan","Jordan","Kazakhstan","Kuwait","Kyrgyzstan","Laos","Lebanon","Malaysia","Maldives","Mongolia","Myanmar","Nepal","North Korea","Oman","Pakistan","Palestine","Philippines","Qatar","Russia","Saudi Arabia","Singapore","South Korea", "Sri Lanka","Syria","Taiwan","Tajikistan","Thailand","Timor-Leste","Turkey","Turkmenistan","United Arab Emirates","Uzbekistan","Vietnam","Yemen"]
+//]
